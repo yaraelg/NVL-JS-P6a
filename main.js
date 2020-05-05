7d651a5252921877a11e2f7600774189496b2aa9
yaraelg <yara.elg95@gmail.com>
2020-05-05 19:28:10 +0200

    he puesto los valores indicados

---
+++ main.js
@@ -0,0 +1,12 @@
+nombre= 'Yara El Lakkis González';
+var nombre= "Yara El Lakkis González";
+
+edad= 25;
+var edad=25 ;
+
+valorgravedad= 9.8;
+var valorgravedad= 9.8;
+
+var dias=["dom", "lun", "mar", "mie", "jue", "vie", "sab"];
+
+var numerosimpares= [1,3,5];
